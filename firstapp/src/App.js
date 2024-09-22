1da2e9f7da0278c24527141c3506b830155bf85d
srshti <srshti2012@gmail.com>
2024-09-22 17:13:39 +0530

    react changes

---
+++ firstapp/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/helloworld';
+import Greeting from './components/greeting';
+import StyleComponent from './components/stylecomponent';
+import ActionButton from './components/actionbutton';
+import NumberList from './components/numberlist';
+import Counter from './components/counter';
+import Toggle from './components/toggle';
+import Form from './components/form';
+import UserInfo from './components/userinfo';
+import ArrayState from './components/arraysstate';
+import Bike from './components/bike';
+
+function App() {
+  return (
+    <div className="container">
+      {/* <HelloWorld/>
+      <Greeting/>
+      <StyleComponent/>
+      <ActionButton/>
+      <NumberList/>
+      <Counter/>
+      <Toggle/> 
+       <Form/>
+       <UserInfo/>
+       <ArrayState/> */}
+
+       
+      <Bike/>
+
+       
+
+     
+
+    </div>
+  );
+}
+
+export default App;
